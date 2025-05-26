bf315a6d9db236a22eca818f08bc13ac9acdc5dc
broniaB2 <boonabaanabronia85@gmail.com>
2025-05-26 16:36:46 +0300

    mthd overloading

---
+++ methodoverloading.py
@@ -0,0 +1,13 @@
+#methods having the same names but different kinds of parameters
+
+def sum(w,r):
+  s= w+r
+  print(s)
+
+
+def sum(w,r,t):
+   s=w+r+t
+   print("sum is:",s)
+
+sum(1,3,2) 
+#sum(1,2) 
