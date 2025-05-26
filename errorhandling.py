65dfeda993aff63f314396d0eb9ee0f050f0b5ea
broniaB2 <boonabaanabronia85@gmail.com>
2025-05-26 12:12:19 +0300

    added the second assignment

---
+++ errorhandling.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+        
+        result = num1/num2
+        print(f"Result:{result}")
+        break 
+    #exit loop if the operation is successfull
+    except ValueError:
+        print("invalid input ! please enter numerical values")
+    
+    except ZeroDivisionError:
+        print("cannot divide by zero .try again")
---
+++ factoriolassignment.py
@@ -0,0 +1,10 @@
+#calculating factorial of 5
+
+#number = n
+def factorial(n):
+     result = 1
+     for i in range(1, n+1): 
+       result *= i
+     return result
+
+print("Factorial of 5 is:",factorial(5))
