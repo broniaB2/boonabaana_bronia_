5233cd03b2b1212e9ac1d564170903a3d797b5c1
broniaB2 <boonabaanabronia85@gmail.com>
2025-06-26 17:25:00 +0300

    web development with django

--- assignment3.py
+++ assignment3.py
@@ -3,17 +3,18 @@
 class Employee:
     def __init__(self, name, employee_id):
         self.name = name
-        self.employee_id = employee_id  
-    
+        self.employee_id = employee_id
+
     def compute_salary(self):
         return 0
-    
+
     def display_info(self):
         print(f"Employee: {self.name} (ID: {self.employee_id})")
 
+
 class MonthlypaidEmployee(Employee):
     def __init__(self, name, employee_id, salary):
-        super().__init__(name, employee_id) 
+        super().__init__(name, employee_id)
         self.salary = salary
 
     # Method overriding on the compute salary method that is calculating the monthly payment
@@ -21,8 +22,9 @@ class MonthlypaidEmployee(Employee):
         return self.salary / 12
 
     def display_info(self):
-        super().display_info() 
+        super().display_info()
         print(f"Type: paid, Annual Salary is: ugx{self.salary:,}")
+
 
 class Per_daypaidEmployee(Employee):
     def __init__(self, name, employee_id, days_worked, daily_rate):
@@ -36,26 +38,32 @@ class Per_daypaidEmployee(Employee):
 
     def display_info(self):
         super().display_info()
-        print(f"Type: per_day, Rate: ugx{self.daily_rate}*days, Days: {self.days_worked}")
+        print(
+            f"Type: per_day, Rate: ugx{self.daily_rate}*days, Days: {self.days_worked}"
+        )
+
 
 class HourlyEmployee(Employee):
     def __init__(self, name, employee_id, hourly_rate, hours_worked):
         super().__init__(name, employee_id)
         self.hourly_rate = hourly_rate
-        self.hours_worked = hours_worked  
+        self.hours_worked = hours_worked
 
-    def compute_salary(self):  
+    def compute_salary(self):
         return self.hourly_rate * self.hours_worked
-    
+
     def display_info(self):
         super().display_info()
-        print(f"Type: Hourly, Rate: ugx{self.hourly_rate}/hr, Hours: {self.hours_worked}")  
+        print(
+            f"Type: Hourly, Rate: ugx{self.hourly_rate}/hr, Hours: {self.hours_worked}"
+        )
+
 
 # this shows MRO
 print("HourlyEmployee MRO:", HourlyEmployee.__mro__)
 
 emp1 = MonthlypaidEmployee("Bronia B", "E01", 2000000)
-emp2 = Per_daypaidEmployee("Bridget", "E02", 20, 90000)  
+emp2 = Per_daypaidEmployee("Bridget", "E02", 20, 90000)
 emp3 = HourlyEmployee("Bruno T", "E03", 5000, 100)
 
 for emp in [emp1, emp2, emp3]:
--- errorhandling.py
+++ errorhandling.py
@@ -2,13 +2,13 @@ while True:
     try:
         num1 = int(input("Enter the first number: "))
         num2 = int(input("Enter the second number: "))
-        
-        result = num1/num2
+
+        result = num1 / num2
         print(f"Result:{result}")
-        break 
-    #exit loop if the operation is successfull
+        break
+    # exit loop if the operation is successfull
     except ValueError:
         print("invalid input ! please enter numerical values")
-    
+
     except ZeroDivisionError:
         print("cannot divide by zero .try again")
